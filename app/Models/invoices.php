fe4bd94c7320180fdc4d101e2c8f666013580d2e
sakerYoucef <youcefsaker09@gmail.com>
2024-07-12 14:51:04 +0100

    inserte invoice

--- app/Models/invoices.php
+++ app/Models/invoices.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class invoices extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'invoice_number',
+        'invoice_Date',
+        'Due_date',
+        'product',
+        'section_id',
+        'Amount_collection',
+        'Amount_Commission',
+        'Discount',
+        'Value_VAT',
+        'Rate_VAT',
+        'Total',
+        'Status',
+        'Value_Status',
+        'note',
+        'Payment_Date',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+
+
+   public function section()
+   {
+       return $this->belongsTo(sections::class, 'section_id');
+   }
 }
--- app/Models/invoices_detaills.php
+++ app/Models/invoices_detaills.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class invoices_detaills extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id_Invoice',
+        'invoice_number',
+        'product',
+        'Section',
+        'Status',
+        'Value_Status',
+        'note',
+        'user',
+        'Payment_Date',
+    ];
 }
